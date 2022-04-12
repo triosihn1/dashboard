2b944749204441b75ada8bcf5997289b9af9a715
triosihn1 <103525307+triosihn1@users.noreply.github.com>
2022-04-13 02:16:22 +0700

    Create sidebar.js

---
+++ tresh/sidebar.js
@@ -0,0 +1,13 @@
+
+//Daftar Item Sidebar
+itemSidebar = [{
+	title : 'Download',
+	icon : 'fas fa-arrow-down',
+	item : [{
+		'url' : '/docs/api/tiktok?query=kontol',
+		'name' : 'Tik Tok Download'
+		}, {
+			'url' : '/docs/api/instagram?query=kontol',
+		'name' : 'Instagram Download'
+	}]
+}]
