faf57fa3a25ea9077f0ebcdfb79c8589a6b6ccc1
triosihn1 <103525307+triosihn1@users.noreply.github.com>
2022-04-14 23:54:34 +0700

    Update sidebar.js

--- dist/js/sidebar.js
+++ dist/js/sidebar.js
@@ -34,30 +34,36 @@ itemSidebar = [{
 	"title" : "Download",
 	"icon" : "fas fa-download",
 	"item" : [{
-		"url" : "/docs/api/tiktok?url=https://vt.tiktok.com/ZSdhTsAWd/",
+		"url" : "/docs/api/download/tiktok?url=https://vt.tiktok.com/ZSdhTsAWd/",
 		"name" : "Tik Tok & Detail Video"
 	}, {
-		"url" : "/docs/api/instagram?url=https://www.instagram.com/p/CMTfYtMjVSb/?igshid=MDJmNzVkMjY=",
+		"url" : "/docs/api/download/instagram?url=https://www.instagram.com/p/CMTfYtMjVSb/?igshid=MDJmNzVkMjY=",
 		"name" : "Instagram reel & post"
 	}, {
-		"url" : "/docs/api/instagram/story?query=triosihn_",
+		"url" : "/docs/api/download/instagram/story?query=triosihn_",
 		"name" : "Instagram story",
 		"event" : "New"
 	}, {
-		"url" : "/docs/api/youtube?url=https://youtu.be/IHMLFPhXleM",
+		"url" : "/docs/api/download/youtube?url=https://youtu.be/IHMLFPhXleM",
 		"name" : "You Tube MP4 & MP3",
 	}, {
-		"url" : "/docs/api/facebook?url=https://www.facebook.com/cartoonboxanimation/videos/1433518997002123/",
+		"url" : "/docs/api/download/facebook?url=https://www.facebook.com/cartoonboxanimation/videos/1433518997002123/",
 		"name" : "Facebook Video & Audio",
 	}, {
-		"url" : "/docs/api/twitter?url=https://twitter.com/PassengersMovie/status/821025484150423557",
+		"url" : "/docs/api/download/twitter?url=https://twitter.com/PassengersMovie/status/821025484150423557",
 		"name" : "Twitter Video & Audio",
+	}, {
+		"url" : "/docs/api/download/like?url=https://likee.video/@vicky_marpaung/video/7006676628722311449",
+		"name" : "Like Video",
+	}, {
+		"url" : "/docs/api/download/pinterest?url=https://pin.it/Nt2fdSb",
+		"name" : "Pinterest Video & Image",
 	}]
 	},{
 	"title" : "search",
 	"icon" : "fas fa-search",
 	"item" : [{
-		"url" : "/docs/api/twitter?url=https://twitter.com/PassengersMovie/status/821025484150423557",
+		"url" : "/docs/api/download/twitter?url=https://twitter.com/PassengersMovie/status/821025484150423557",
 		"name" : "Test",
 		"error" : "Test Doang"
 	}]
