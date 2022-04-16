2b953c352f1beb4958e52ac20fd6c1c4d8d2d72c
triosihn1 <103525307+triosihn1@users.noreply.github.com>
2022-04-17 00:43:07 +0700

    Update sidebar.js

--- dist/js/sidebar.js
+++ dist/js/sidebar.js
@@ -111,7 +111,7 @@ itemSidebar = [{
 	},{
 	"title" : "photooxy",
 	"icon" : "fas fa-image",
-	"event": "Scrape",
+	
 	"item" : [{
 		"url" : "/docs/api/photooxy/tiktok/effect?text1=triosihn_&text2=api%20dari%20bang%20hadi",
 		"name" : "Tik Tok Effect",
